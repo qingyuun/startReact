c7c14d9e3f8620e39df44d2eea4d0356689792bf
綪蕓 <zheng2223021713@hotmail.com>
2018-08-16 14:47:05 +0800

    同时使用 npm install 和yarn add 两个包管理器 自动引入antd相关样式配置

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {Row,Col,Button} from 'antd'
 import './App.css';
-import { Row, Col } from 'antd';
 
 class App extends Component {
   render() {
@@ -14,17 +14,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">left 3</div>
-            <div className="col-md-11">right 9</div>
-          </div>
-        </div>
+        <Button type="primary">button</Button>
         <Row>
-          <Col span={4}>col-4</Col>
-          <Col span={6} offset={8}>
-            col-8
-          </Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
+          <Col span={6}>col-6</Col>
           <Col span={6}>col-6</Col>
         </Row>
       </div>
