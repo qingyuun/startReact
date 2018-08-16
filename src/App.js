b3030b4d393e81a9121445a05d77e6e0e6d79a79
綪蕓 <zheng2223021713@hotmail.com>
2018-08-16 14:55:36 +0800

    样式表启用less,卸载sass包

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Row,Col,Button} from 'antd'
-import './App.css';
+import './App.less';
 
 class App extends Component {
   render() {
