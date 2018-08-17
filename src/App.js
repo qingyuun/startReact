a8a7f7f617a0b11a63e70e471eb04fad2c8ee8f3
綪蕓 <zheng2223021713@hotmail.com>
2018-08-17 16:45:00 +0800

    导航栏添加默认选中

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route,Switch } from 'react-router-dom'
-import { Layout, Menu,Breadcrumb, Icon } from 'antd'
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd'
 import './App.less';
 import Home from "./view/home";
 import About from "./view/about";
@@ -10,70 +10,85 @@ const { Header, Sider, Content } = Layout
 
 class App extends Component {
   state = {
-    collasped: false
-  };
+    collasped: false,
+    menuKey: '/',
+  }
   toogle = () => {
     this.setState({
       collasped: !this.state.collasped
     })
-  };
+  }
+  toLink=(e)=>{
+    this.setState({
+      menuKey:e.key
+    })
+  }
+  //页面渲染前
+  componentWillMount() {
+    this.setState({
+      menuKey:window.location.pathname
+    })
+  }
+  //页面渲染完成
+  componentDidMount() {
+
+  }
+  //页面更新前
+  componentWillUpdate(newprops, oldprops) {
+    console.log(newprops, oldprops)
+  }
+  //页面更新完成
+  componentDidUpdate(newprops, oldprops) {
+    console.log(newprops, oldprops)
+  }
+  //页面销毁前
+  componentWillUnmount() {
+
+  }
   render() {
     return (
-      <Layout>
-        <Sider
-          trigger={null}
-          collapsible
-          collapsed={this.state.collasped}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span>nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span>nav 3</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
+      <BrowserRouter>
         <Layout>
-          <Header>
-            <Icon
-              className="trigger"
-              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-              onClick={this.toogle}
-            />
-            {/* <Menu
-              theme="dark"
-              mode="horizontal"
-              defaultSelectedKeys={['2']}
-              style={{ lineHeight: '64px',float:'left' }}
-            >
-              <Menu.Item key="1">nav 1</Menu.Item>
-              <Menu.Item key="2">nav 2</Menu.Item>
-              <Menu.Item key="3">nav 3</Menu.Item>
-            </Menu> */}
-          </Header>
-          <Breadcrumb>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <Content>
-            <BrowserRouter>
-                <Switch>
-                  <Route path="/" component={Home} exact />
-                  <Route path="/about" component={About} />
-                  <Route component={ErrorPage} />
-                </Switch>
-            </BrowserRouter>
-          </Content>
+          <Sider trigger={null} collapsible collapsed={this.state.collasped}>
+            <div className="logo" />
+            <Menu theme="dark" mode="inline" selectedKeys={[this.state.menuKey]} onClick={this.toLink}>
+              <Menu.Item key="/">
+                <NavLink to="/" replace>
+                  <Icon type="user" />
+                  <span>Home</span>
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="/about">
+                <NavLink to="/about" replace>
+                  <Icon type="video-camera" />
+                  <span>About</span>
+                </NavLink>
+              </Menu.Item>
+              <Menu.Item key="3">
+                <Icon type="upload" />
+                <span>nav 3</span>
+              </Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout>
+            <Header>
+              <Icon className="trigger" type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} onClick={this.toogle} />
+            </Header>
+            <Breadcrumb>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content>
+              <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/about" component={About} />
+                <Route component={ErrorPage} />
+              </Switch>
+            </Content>
+          </Layout>
         </Layout>
-      </Layout>
+      </BrowserRouter>
     );
   }
 }
